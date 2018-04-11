a2a502c7735eefed6a364c402254296193730b95
Błażej Matuszewski <blazej.matuszewski@corp.smtsoftware.com>
2018-04-11 16:04:25 +0200

    2 initial projects

---
+++ 01_HelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace New_folder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //System.Console.
+            System.Console.WriteLine("Hello World!2");
+        }
+    }
+}
---
+++ 02_BaseLine/TheDevice.cs
@@ -0,0 +1,73 @@
+using System.Drawing;
+
+namespace TheDevice
+{
+    public static class TheDevice
+    {
+        static int width = 30;
+        static int height = 5;
+        static byte[] data;
+
+        static TheDevice()
+        {
+            data = new byte[width*height];
+            for(int i=0;i<width*height;i++)
+            {
+                data[i] = 0;
+            }
+        }
+
+        public static int GetWidth()
+        {
+            return width;
+        }
+
+        public static int GetHeight()
+        {
+            return width;
+        }
+
+        public static void SetPixel(int x, int y, bool on)
+        {
+            if(x>=0 && x<width && y>=0 && y<height )
+            {
+                data[x + y*width] = on ? (byte)1:(byte)0;
+            }
+        }
+
+        public static void Visualize()
+        {
+            System.Console.WriteLine($"{width}x{height}:");
+
+            // TOP
+            System.Console.Write("┌"); 
+            for(int x=0;x<width;x++)
+            {
+               System.Console.Write("─"); 
+            }
+            System.Console.WriteLine("┐"); 
+
+            // MIDDLE
+            for(int y=0;y<height;y++)
+            {
+                System.Console.Write("│");
+        
+                for(int x=0;x<width;x++)
+                {
+                    System.Console.Write(data[x + y*width]==0 ? " " : "*");
+                }
+                System.Console.Write("│");
+                System.Console.WriteLine();
+            }
+
+            // BOTTOM
+            System.Console.Write("└"); 
+            for(int x=0;x<width;x++)
+            {
+               System.Console.Write("─"); 
+            }
+            System.Console.WriteLine("┘");            
+
+        }
+    }
+}
