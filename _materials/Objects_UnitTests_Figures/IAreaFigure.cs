b9feaeb9015ef81dada88c7a06e27b7a506d16a6
Błażej Matuszewski <blazej.matuszewski@corp.smtsoftware.com>
2018-04-13 09:48:47 +0200

    moving materials to materials branch only

--- _materials/Objects_UnitTests_Figures/IAreaFigure.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Figures
-{
-    public interface IAreaFigure
-    {
-        int GetArea();
-    }
-}
--- _materials/SeleniumPOFramework/SeleniumFinal/Core/Consts.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SeleniumFinal.Core
-{
-    internal static class Consts
-    {
-        public const string UserLogin = "chom109";
-        public const string UserPassword = "109chOM";
-        public const string StartPageUrl = "http://chomikuj.pl";
-    }
-}
