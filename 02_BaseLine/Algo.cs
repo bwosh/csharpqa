086782fa64b918ac3c8b121acdbf15c5cef451d0
Błażej Matuszewski <blazej.matuszewski@corp.smtsoftware.com>
2018-04-20 09:29:57 +0200

    Prime numbers

--- 02_BaseLine/Algo.cs
+++ 02_BaseLine/Algo.cs
@@ -3,26 +3,34 @@ public static class Algo
     public static void Run()
     {
         // 1. Prime numbers: 1-100
-        for(int i=0;i<100;i++)
+        for (int i = 2; i < 100; i++)
         {
-            System.Console.Write(i);
-            System.Console.Write(' ');
-            // TODO....
+            bool isPrime = true;
+            for (int j = 2; j < i; j++)
+            {
+                if (i % j == 0)
+                {
+                    isPrime = false;
+                    break; // optional
+                }
+            }
+
+            if (isPrime)
+            {
+                System.Console.Write(i);
+                System.Console.Write(' ');
+            }
         }
 
-        // 2. Greatest common divisor GCD(6,10)=2
-
-        // 3. Least common multiple LCD(6,10)=30
-
-        // 4. Fibbonaci numbers < 100
-
         // 1,2,3,4 as methods (variable visibility, parameters, output value, recursion)
-
         // The device : Sierpinski carpet (recursion on graph), change the device.
 
+        // HW. (method) Greatest common divisor GCD(6,10)=2
+        // HW. (method) Least common multiple LCD(6,10)=30
+        // HW. (recursion method) Fibbonaci numbers < 100 : f(1) = 1, f(2) = 1, f(n)=f(n-1)+f(n-2)
 
-        // HW: Highest prime number challange (long)
-        // HW: Highest number with maximum number of divisors
+        // HW: (challange) Highest prime (long) number challange 
+        // HW: (chalange) Highest number (long) with maximum number of divisors
         // HW(Hard): The Device: Fibbonaci numbers
         // HW(Hard): The Device: Ulam spiral (prime numbers)
 
